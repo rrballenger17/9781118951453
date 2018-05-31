a3deaed6083a165c4ffe46bcf6b7ef3911a47cb9
Ryan Ballenger <ryanballenger@38.122.127.226.wireless-guest.net.tufts.edu>
2018-05-31 19:19:31 -0400

    abstract class example

---
+++ Lesson7/TestPayIncrease.java
@@ -0,0 +1,16 @@
+
+
+public class TestPayIncrease {
+
+      public static void main(String[] args) {
+
+        Person workers[] = new Person[3];
+              workers[0] = new Employee("John");
+              workers[1] = new Contractor("Mary");
+              workers[2] = new Employee("Steve");
+
+              for (Person p: workers){
+                        p.promote(30);
+              }
+        }
+}
